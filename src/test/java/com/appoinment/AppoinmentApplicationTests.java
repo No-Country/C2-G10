35fbf40bc1868a8fb6ba54685a246acd8f6efaff
Felipe Herrera <herrerafelipe.093@gmail.com>
2022-01-07 10:33:15 -0300

    First Commit

---
+++ src/test/java/com/appoinment/AppoinmentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.appoinment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppoinmentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
